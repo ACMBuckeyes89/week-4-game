d0d4584237c3962b83b6f1925dee07eacb6a2f5b
Andy Landaverde <ForzaMilan@Andys-MBP.att.net>
2017-09-14 19:26:51 -0700

    Fixed issues with previous javascript file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,18 +3,28 @@ $(document).ready(function(){
 
 // Creating random numbers that will be assigned to the crystals
 // Numbers have to be between 1-12
-function crystalValues() {
 var crystalOne = Math.floor((Math.random() * 12) + 1);
 var crystalTwo = Math.floor((Math.random() * 12) + 1);
 var crystalThree = Math.floor((Math.random() * 12) + 1);
 var crystalFour = Math.floor((Math.random() * 12) + 1); 
 //Testing the variables above
 console.log(crystalOne);
+console.log(crystalTwo);
+console.log(crystalThree);
+console.log(crystalFour);
+//Now I call the variables inside a function
+function crysStyle(){
+	 var crystalOne;
+	 var crystalTwo;
+	 var crystalThree;
+	 var crystalFour;
 }
+crysStyle();
 
 //Win and Loss column 
 var wins = 0;
 var losses = 0;
+
 //Adding wins and losses to html 
 //document.getElementById("keepScore").innerHTML = "Wins: " + wins + "<br> Loss: " + losses;
 $('#triumph').text(wins);
@@ -23,24 +33,27 @@ $('#defeat').text(losses);
 //Creating a random number that will appear in the random number box.
 function maskedNum(){
 var randomNum = Math.floor((Math.random() * 120) + 19);
+
 //Creating a console.log to test the that randomNum works
 console.log(randomNum); 
 //Adding randomNum to html 
 $('#ranNum').text(randomNum);
 }
+maskedNum();
 
 //Adding a variable for beginning total user score to html
 //userScore will also serve as the counter
-function playerScore() {
-var userScore = 0; 
+var userScore = 0;
 $('#totalScore').text(userScore);
-}
 
 // Reseting game 
 function reset() {
 	maskedNum();
-	crystalValues();
-	playerScore();
+	//Reseting crystal values
+	crysStyle();
+	//Reseting userScore
+	var userScore = 0;
+	$('#totalScore').text(userScore);
 }
 
 //Functions when player has won or lost 
